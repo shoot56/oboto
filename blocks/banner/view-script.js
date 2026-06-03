c3c4c45a3e225e5e54b803f06e21807c6d2343e5
Andrii Maslov <pandafly@gmail.com>
2026-06-03 14:58:12 +0200

    SEO optimisation

--- blocks/banner/view-script.js
+++ blocks/banner/view-script.js
@@ -1,12 +1,14 @@
-
 document.addEventListener("DOMContentLoaded", function () {
   const banner = document.querySelector(".banner");
 
   if (banner) {
     const closeBanner = banner.querySelector("button");
-    closeBanner.addEventListener("click", () => {
-      banner.remove();
-    });
+
+    if (closeBanner) {
+      closeBanner.addEventListener("click", () => {
+        banner.remove();
+      });
+    }
 
     setTimeout(() => {
       banner.remove();
